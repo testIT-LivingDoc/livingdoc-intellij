764166567f85b7e0381921a2ea111c928cee2196
mruiz <mruizsantaella@gmail.com>
2016-09-14 09:29:21 +0200

    Method to create formated titles in the settings/preferences screen

--- src/main/java/info/novatec/testit/livingdoc/intellij/ui/UIUtils.java
+++ src/main/java/info/novatec/testit/livingdoc/intellij/ui/UIUtils.java
@@ -1,6 +1,9 @@
 package info.novatec.testit.livingdoc.intellij.ui;
 
+import com.intellij.ui.IdeBorderFactory;
+import com.intellij.ui.border.IdeaTitledBorder;
 import com.intellij.ui.components.JBLabel;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
@@ -26,5 +29,20 @@ public class UIUtils {
         constraints.gridwidth = totalColumns;
         constraints.gridheight = 1;
         jPanel.add(new JBLabel("\n"), constraints);
+    }
+
+    /**
+     * Returns a title with the following format (e.g): 'title'_____________________
+     *
+     * @param title String
+     * @return {@link IdeaTitledBorder}
+     */
+    @NotNull
+    public static IdeaTitledBorder createTitledBorder(@NotNull final String title) {
+        return IdeBorderFactory.createTitledBorder(title, true,
+                new Insets(IdeBorderFactory.TITLED_BORDER_TOP_INSET,
+                        0,
+                        IdeBorderFactory.TITLED_BORDER_BOTTOM_INSET,
+                        IdeBorderFactory.TITLED_BORDER_RIGHT_INSET));
     }
 }
